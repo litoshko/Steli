5b08e0bf1336271443061f4fa73976ce9f1e5bf2
xxoro <xxoro@mail.ru>
2015-02-08 15:55:59 +0200

    Initial commit

---
+++ Steli/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Steli.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Замовляйте натяжні стелі у нас.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Зв'яжіться з нами.";
+
+            return View();
+        }
+
+        public ActionResult Prices()
+        {
+            ViewBag.Message = "Зв'яжіться з нами.";
+
+            return View();
+        }
+
+        public ActionResult Offers()
+        {
+            ViewBag.Message = "Зв'яжіться з нами.";
+
+            return View();
+        }
+    }
+}
